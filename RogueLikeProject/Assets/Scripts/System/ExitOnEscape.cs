e353cf1ef0ee36d378def001f0e349c995046800
niboshino514 <niboshino514@gmail.com>
2026-05-19 18:42:46 +0900

    escapeキーを押した際に、ゲームを強制終了する機能追加

---
+++ RogueLikeProject/Assets/Scripts/System/ExitOnEscape.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace System
+{
+    public class ExitOnEscape : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Keyboard.current.escapeKey.wasPressedThisFrame)
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+            }
+        }
+    }
+}
