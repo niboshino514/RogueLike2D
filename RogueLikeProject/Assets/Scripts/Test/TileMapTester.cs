0cfa60dfc5a4e0c35dcefc83d53b92dbb3d41067
niboshino514 <niboshino514@gmail.com>
2026-05-21 17:10:21 +0900

    Camera調整中

--- RogueLikeProject/Assets/Scripts/Test/TileMapTester.cs
+++ RogueLikeProject/Assets/Scripts/Test/TileMapTester.cs
@@ -1,4 +1,5 @@
 ﻿using MoreMountains.CorgiEngine;
+using NUnit.Framework.Internal;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -50,16 +51,67 @@ public class TilemapBoundsDrawer : MonoBehaviour
         Vector3 minWorld = tilemap.CellToWorld(new Vector3Int(minX, minY, 0));
         Vector3 maxWorld = tilemap.CellToWorld(new Vector3Int(maxX + 1, maxY + 1, 0));
 
-        // 境界を作成
-        worldBounds = new Bounds();
+
+        Vector2 cameraWorldSize = GetCameraWorldSize();
+        Vector2 min = new Vector2(-(cameraWorldSize.x * 0.5f), -(cameraWorldSize.y * 0.5f));
+        Vector2 max = new Vector2((cameraWorldSize.x * 0.5f), (cameraWorldSize.y * 0.5f));
+        Debug.Log(minWorld);
+        Debug.Log(maxWorld);
+        Debug.Log(min);
+        Debug.Log(max);
+
+        if (minWorld.x >= min.x)
+        {
+            minWorld.x = min.x;
+        }
+        if (maxWorld.x <= max.x)
+        {
+            maxWorld.x = max.x;
+        }
+        if(minWorld.y >= min.y)
+        {
+            minWorld.y = min.y; 
+        }
+        if (maxWorld.y <= max.y)
+        {
+            maxWorld.y = max.y;
+        }
+
+
+            // 境界を作成
+            worldBounds = new Bounds();
         worldBounds.SetMinMax(minWorld, maxWorld);
 
         _levelManager.LevelBounds = worldBounds;
     }
 
+    public static Vector2 GetCameraWorldSize()
+    {
+        Camera cam = Camera.main;
+
+        float height = cam.orthographicSize * 2f;
+        float width = height * cam.aspect;
+
+        return new Vector2(width, height);
+    }
+
     void OnDrawGizmos()
     {
         if (tilemap == null) return;
+        Vector2 test = GetCameraWorldSize();
+        Vector2 min = new Vector2(-(test.x * 0.5f), -(test.y * 0.5f));
+        Vector2 max = new Vector2((test.x * 0.5f), (test.y * 0.5f));
+
+        Vector2 leftTop = new Vector2(min.x, max.y);
+        Vector2 leftBottom = new Vector2(min.x, min.y);
+        Vector2 rightTop = new Vector2(max.x, max.y);
+        Vector2 rightBottom = new Vector2(max.x, min.y);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(leftTop, leftBottom);
+        Gizmos.DrawLine(leftBottom, rightBottom);
+        Gizmos.DrawLine(rightBottom, rightTop);
+        Gizmos.DrawLine(rightTop, leftTop);
 
         CalculateBounds();
 
