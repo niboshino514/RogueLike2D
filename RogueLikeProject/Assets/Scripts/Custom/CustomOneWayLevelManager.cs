e5c73babb12ab408c6b904030dcc9cd6db73e72f
niboshino514 <niboshino514@gmail.com>
2026-06-01 12:32:32 +0900

    cinemaシーンカメラ調整始め

--- RogueLikeProject/Assets/Scripts/Custom/CustomOneWayLevelManager.cs
+++ RogueLikeProject/Assets/Scripts/Custom/CustomOneWayLevelManager.cs
@@ -1,80 +1,80 @@
-﻿using UnityEngine;
-using MoreMountains.CorgiEngine;
+﻿using MoreMountains.CorgiEngine;
+using Sirenix.OdinInspector;
+using UnityEngine;
 
 public class CustomOneWayLevelManager : OneWayLevelManager
 {
-    
-    private Bounds _originalBounds;
-    private bool _initialized = false;
+    //private Bounds _originalBounds;
+    //private bool _initialized = false;
 
-    protected override void Initialization()
-    {
-        base.Initialization();
+    //protected override void Initialization()
+    //{
+    //    base.Initialization();
 
-        if (!_initialized)
-        {
-            _originalBounds = LevelManager.Instance.LevelBounds;
-            _initialized = true;
-        }
-    }
+    //    if (!_initialized)
+    //    {
+    //        _originalBounds = LevelManager.Instance.LevelBounds;
+    //        _initialized = true;
+    //    }
+    //}
 
-    // ★ StageManager が Bounds を設定した後に呼ぶ
-    public void ApplyPreventGoingBack(Vector2 playerPos)
-    {
-        base.Initialization(); // 念のため初期化
+    //// ★ StageManager が Bounds を設定した後に呼ぶ
+    //public void ApplyPreventGoingBack(Vector2 playerPos)
+    //{
+    //    base.Initialization(); // 念のため初期化
 
-        HandlePreventGoingBack_Manual(playerPos);
-    }
+    //    HandlePreventGoingBack_Manual(playerPos);
+    //}
 
-    // ★ OneWayLevelManager の HandlePreventGoingBack を外部から呼べるように再構築
-    private void HandlePreventGoingBack_Manual(Vector2 playerPos)
-    {
-        MinBounds = LevelManager.Instance.LevelBounds.min;
-        MaxBounds = LevelManager.Instance.LevelBounds.max;
+    //// ★ OneWayLevelManager の HandlePreventGoingBack を外部から呼べるように再構築
+    //private void HandlePreventGoingBack_Manual(Vector2 playerPos)
+    //{
+    //    MinBounds = LevelManager.Instance.LevelBounds.min;
+    //    MaxBounds = LevelManager.Instance.LevelBounds.max;
 
-        switch (OneWayLevelDirection)
-        {
-        case OneWayLevelDirections.Right:
-            MinBounds.x = Mathf.Max(playerPos.x - ThresholdDistance, _minBoundsLastFrame.x);
-            break;
+    //    switch (OneWayLevelDirection)
+    //    {
+    //    case OneWayLevelDirections.Right:
+    //        MinBounds.x = Mathf.Max(playerPos.x - ThresholdDistance, _minBoundsLastFrame.x);
+    //        break;
 
-        case OneWayLevelDirections.Left:
-            MaxBounds.x = Mathf.Min(playerPos.x + ThresholdDistance, _maxBoundsLastFrame.x);
-            break;
+    //    case OneWayLevelDirections.Left:
+    //        MaxBounds.x = Mathf.Min(playerPos.x + ThresholdDistance, _maxBoundsLastFrame.x);
+    //        break;
 
-        case OneWayLevelDirections.Up:
-            MinBounds.y = Mathf.Max(playerPos.y - ThresholdDistance, _minBoundsLastFrame.y);
-            break;
+    //    case OneWayLevelDirections.Up:
+    //        MinBounds.y = Mathf.Max(playerPos.y - ThresholdDistance, _minBoundsLastFrame.y);
+    //        break;
 
-        case OneWayLevelDirections.Down:
-            MaxBounds.y = Mathf.Min(playerPos.y + ThresholdDistance, _maxBoundsLastFrame.y);
-            break;
-        }
+    //    case OneWayLevelDirections.Down:
+    //        MaxBounds.y = Mathf.Min(playerPos.y + ThresholdDistance, _maxBoundsLastFrame.y);
+    //        break;
+    //    }
 
-        LevelManager.Instance.SetNewMinLevelBounds(MinBounds);
-        LevelManager.Instance.SetNewMaxLevelBounds(MaxBounds);
+    //    LevelManager.Instance.SetNewMinLevelBounds(MinBounds);
+    //    LevelManager.Instance.SetNewMaxLevelBounds(MaxBounds);
 
-        _minBoundsLastFrame = LevelManager.Instance.LevelBounds.min;
-        _maxBoundsLastFrame = LevelManager.Instance.LevelBounds.max;
-    }
+    //    _minBoundsLastFrame = LevelManager.Instance.LevelBounds.min;
+    //    _maxBoundsLastFrame = LevelManager.Instance.LevelBounds.max;
+    //}
 
-    public void SyncInternalBoundsWithLevelManager()
-    {
-        Bounds b = LevelManager.Instance.LevelBounds;
+    //public void SyncInternalBoundsWithLevelManager()
+    //{
+    //    Bounds b = LevelManager.Instance.LevelBounds;
 
-        MinBounds = b.min;
-        MaxBounds = b.max;
+    //    MinBounds = b.min;
+    //    MaxBounds = b.max;
 
-        _minBoundsLastFrame = b.min;
-        _maxBoundsLastFrame = b.max;
-    }
+    //    _minBoundsLastFrame = b.min;
+    //    _maxBoundsLastFrame = b.max;
+    //}
 
 
-    // ★ Reset（元の Bounds に戻す）
-    public void ResetBounds()
-    {
-        if (!_initialized) return;
+    //// ★ Reset（元の Bounds に戻す）
+    //public void ResetBounds()
+    //{
+    //    if (!_initialized) return;
 
-        LevelManager.Instance.SetNewLevelBounds(_originalBounds);
-    }
+    //    LevelManager.Instance.SetNewLevelBounds(_originalBounds);
+    //}
 }
--- RogueLikeProject/Assets/Scripts/TileMap/StageScroller.cs
+++ RogueLikeProject/Assets/Scripts/TileMap/StageScroller.cs
@@ -6,91 +6,96 @@ using UnityEngine;
 using Utility.Core;
 using static MoreMountains.CorgiEngine.OneWayLevelManager;
 
+[AddComponentMenu("ステージのスクロール設定")]
 public class StageScroller : SingletonMonoBehaviour<StageScroller>
 {
     /// <summary>
-    /// ステージスクロール情報
+    /// スクロールタイプ
     /// </summary>
-    [Serializable]
-    public class StageScrollInfo
+    public enum ScrollType
     {
-        /// <summary>
-        /// ステージの進行方向
-        /// </summary>
-        [LabelText("ステージの進行方向")]
-        [LabelWidth(200)]
-        [Tooltip("スクロールを行わないのなら[未設定]\n右スクロール(右に進むステージ)なら [右]に設定\netc...")]
-        public OneWayLevelDirections oneWayLevelDirection = OneWayLevelDirections.None;
-
-        /// <summary>
-        /// 進行方向と逆方向への移動無効化
-        /// </summary>
-        [HideIf(nameof(oneWayLevelDirection), OneWayLevelDirections.None)]
-        [LabelText("進行方向と逆方向への移動無効化")]
-        [LabelWidth(200)]
-        [Tooltip("✓を入れると、\nプレイヤーが進行方向と逆方向に戻れなくなります")]
-        public bool preventGoingBack = true;
-
-        /// <summary>
-        /// 戻り禁止の境界距離
-        /// </summary>
-        [ShowIf(nameof(IsDrawThresholdDistance))]
-        [LabelText("戻り禁止の境界距離")]
-        [LabelWidth(200)]
-        [Tooltip("戻り禁止の境界がプレイヤーからどれだけ離れるかの距離")]
-        [Range(0.5f, 10.0f)]
-        public float thresholdDistance = 5f;
-
-        /// <summary>
-        /// thresholdDistanceをInspector上で表示するための条件
-        /// </summary>
-        /// <returns></returns>
-        private bool IsDrawThresholdDistance()
-        {
-            if (oneWayLevelDirection != OneWayLevelDirections.None &&
-                preventGoingBack)
-            {
-                return true;
-            }
-
-            return false;
-        }
+        [InspectorName("一方通行")]
+        OneWay,
+        [InspectorName("自動")]
+        Auto
     }
 
     /// <summary>
-    /// スクロール情報
+    /// ステージの進行方向
     /// </summary>
-    [LabelText("スクロール設定"),SerializeField]
-    private StageScrollInfo _stageScrollInfo;
+    [LabelText("ステージの進行方向"),SerializeField]
+    [LabelWidth(200)]
+    [Tooltip("強制スクロールを行わないのなら[未設定]\n右スクロール(右に進むステージ)なら[右]\netc...")]
+    private OneWayLevelDirections _oneWayLevelDirection = OneWayLevelDirections.None;
 
- 
+    /// <summary>
+    /// スクロールタイプ
+    /// </summary>
+    [HideIf(nameof(_oneWayLevelDirection), OneWayLevelDirections.None)]
+    [LabelText("スクロールを行う際のタイプ"), SerializeField]
+    [LabelWidth(200), Indent(1)]
+    [Tooltip("[一方通行]：進行方向と反対に戻れなくなる(初代マリオ等)\n[自動]：進行方向に向かって自動スクロール")]
+    private ScrollType _scrollType;
 
-    [Header("オートスクロール設定")]
+    /// <summary>
+    /// 自動スクロールの速度
+    /// </summary>
+    [ShowIf(nameof(IsDrawAutoScrollSpeed))]
+    [LabelText("自動スクロールの速度")]
+    [LabelWidth(200), Indent(1)]
+    [MinValue(0)]
+    [Tooltip("速度の最小値は0")]
+    public float _autoScrollSpeed = 1f;
 
-    /// if this is true, the level bounds will be modified so that the level auto scrolls towards the OneWayLevelDirection
-    [Header("true にすると、ステージが自動でスクロールします")]
-    public bool OneWayLevelAutoScrolling = false;
-    /// the main camera to use to compute the size of the modified constrained bounds
-    [MMCondition("OneWayLevelAutoScrolling", true)]
-    [Header("オートスクロール時に使用するメインカメラ")]
-    public Camera MainCamera;
-    /// the speed at which the level should auto scroll
-    [MMCondition("OneWayLevelAutoScrolling", true)]
-    [Header("オートスクロールの速度")]
-    public float OneWayLevelAutoScrollingSpeed = 1f;
-    
+    /// <summary>
+    /// 戻り禁止の境界距離
+    /// </summary>
+    [ShowIf(nameof(IsDrawThresholdDistance))]
+    [LabelText("戻り禁止の境界距離")]
+    [LabelWidth(200), Indent(1)]
+    [Range(0.5f, 10.0f)]
+    [Tooltip("戻り禁止の境界がプレイヤーからどれだけ離れるかの距離")]
+    public float _thresholdDistance = 5f;
 
-    public CustomLevelManager _customLevelManager; 
+    /// <summary>
+    /// _autoScrollSpeedをInspector上で表示するための条件
+    /// </summary>
+    /// <returns></returns>
+    private bool IsDrawAutoScrollSpeed()
+    {
+        return _oneWayLevelDirection != OneWayLevelDirections.None && _scrollType == ScrollType.Auto;
+    }
+
+    /// <summary>
+    /// _thresholdDistanceをInspector上で表示するための条件
+    /// </summary>
+    /// <returns></returns>
+    private bool IsDrawThresholdDistance()
+    {
+        return _oneWayLevelDirection != OneWayLevelDirections.None && _scrollType == ScrollType.OneWay;
+    }
+
+    public CustomLevelManager _customLevelManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 test = _customLevelManager.LevelBounds.center;
+        test.x += (_autoScrollSpeed * Time.deltaTime);
+        Bounds bounds = _customLevelManager.LevelBounds;
+        bounds.center = test;
+
+        _customLevelManager.LevelBounds = bounds;
     }
 }
